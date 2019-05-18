56f6068d42597e7a49da57a3c0a959084397f22a
mac <ali.qadomy@hotmail.com>
2019-05-18 13:39:26 +0300

    Add booking tickets form

--- app/src/main/java/com/example/bus_uni/BookingSeat.java
+++ app/src/main/java/com/example/bus_uni/BookingSeat.java
@@ -1,11 +1,16 @@
 package com.example.bus_uni;
 
 import android.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class BookingSeat extends AppCompatActivity {
+
+
+    TextView passengerName, orignLocation, destinationLocation, leavingTime, arrivalTime, busNumber, gateNumber, seatNumber, driverName, driverPhone, companyName, rfidNumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,14 +18,24 @@ public class BookingSeat extends AppCompatActivity {
         setContentView(R.layout.activity_booking_seat);
 
 
-
-
-
-
-
-
+        passengerName = (TextView) findViewById(R.id.textViewPassengerName);
+        orignLocation = (TextView) findViewById(R.id.textViewOriginLocation);
+        destinationLocation = (TextView) findViewById(R.id.textViewDestinationLocation);
+        leavingTime = (TextView) findViewById(R.id.textViewLeavingTime);
+        arrivalTime = (TextView) findViewById(R.id.textViewArrivalTime);
+        busNumber = (TextView) findViewById(R.id.textViewBusNumber);
+        gateNumber = (TextView) findViewById(R.id.textViewGate);
+        seatNumber = (TextView) findViewById(R.id.textViewSeat);
+        driverName = (TextView) findViewById(R.id.driverName_bookingSeat);
+        driverPhone = (TextView) findViewById(R.id.driverPhone_bookingSeat);
+        companyName = (TextView) findViewById(R.id.companyName_bookingSeat);
+        rfidNumber = (TextView) findViewById(R.id.rfidNumber_bookingSeat);
     }
 
+
+
+    //
+    ////
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.home) {
             ActionBar actionBar = getActionBar();
